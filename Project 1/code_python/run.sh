ce76975e7c7c81e6a5e3d0086ac25105991eb1e4
Yorick de Boer <mail@yorickdeboer.nl>
2018-02-18 13:21:31 +0100

    [code1] fix run.sh

--- Project 1/code_python/run.sh
+++ Project 1/code_python/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo apt install libpcap-dev python3-pip
+sudo apt -y install libpcap-dev python3-pip
 
-sudo pip3 install pycap
+sudo pip3 install pypcap
 
 python3 pcap_dns.py $1 $2
