4b0ebe3548682768836d49d4050c294d52d45862
Yorick de Boer <mail@yorickdeboer.nl>
2018-02-22 18:56:36 +0100

    [code2] fix tests

--- Project 2/modules/arp/arp_module.py
+++ Project 2/modules/arp/arp_module.py
@@ -1,49 +1,80 @@
+from pathlib import Path
+from collections import defaultdict
+
+from scapy.layers.l2 import ARP, Ether
+
 from ips_module import IPSModule
 from ips_response import *
-from scapy.layers.l2 import ARP, Ether
 from util import is_valid_mac_address
-
-ARP_ACCESS_CONTROL_LIST = {}  # TODO replace with separate file, should allow more than 1 MAC address per IP
+from ips_logger import log
 
 
 class ARPModule(IPSModule):
+
+    def __init__(self, acl_conf: (Path, None)) -> None:
+        if acl_conf:
+            self.acl = self._parse_acl_config(acl_conf)
+        else:
+            self.acl = None
+
+        super().__init__()
+
     def receive_packet(self, pkt) -> IPSResponse:
         if not self.has_valid_arp_src_mac_address(pkt):
-            return ErrorResponse('a')
+            return ErrorResponse('Packet has invalid ARP source MAC address', {})
 
         if not self.has_valid_arp_dst_mac_address(pkt):
-            return ErrorResponse('a')
+            return ErrorResponse('Packet has invalid ARP destination MAC address', {})
 
-        if not self.response_is_send_to_unicast(pkt):
-            return NoticeRespone('a')
+        if self.acl and not self.packet_is_in_acl(pkt):
+            return ErrorResponse('MAC address is not in ACL', {})
 
-        if not self.request_is_send_to_broadcast(pkt):
-            return NoticeRespone('a')
+        if pkt[ARP].op == ARP.who_has:  # Request
+            if not self.request_is_send_to_broadcast(pkt):
+                return NoticeRespone('ARP Request is not to broadcast', {})
+            if not self.request_linklayer_address_matches_arp(pkt):
+                return NoticeRespone('Link layer MAC does not match ARP response MAC', {})
+        else:  # Response
+            if self.response_has_ip_bind_to_mac_broadcast(pkt):
+                return ErrorResponse('ARP response tries to bind IP to MAC broadcast', {})
+            if not self.response_is_send_to_unicast(pkt):
+                return NoticeRespone('ARP Response is not to unicast', {})
+            if not self.response_linklayer_address_matches_arp(pkt):
+                return NoticeRespone('Link layer MAC does not match ARP response MAC', {})
 
-        if not self.linklayer_address_matches_arp(pkt):
-            return NoticeRespone('a')
+        return PermittedResponse('Packet is all good', {})
 
-        return PermittedResponse('All permitted')
+    def _parse_acl_config(self, acl_config: Path) -> dict:
+        with acl_config.open() as f:
+            acl = defaultdict(list)
+            for idx, l in enumerate(f):
+                try:
+                    ip = l.split(' ')[0]
+                    mac = l.split(' ')[1]
+                    acl[ip].append(mac)
+                except KeyError:
+                    log.error('ARP config failed to parse line {:d}'.format(idx))
+        return acl
 
-    def linklayer_address_matches_arp(self, pkt):
-        if pkt[ARP].op == ARP.is_at and pkt[ARP].hwsrc == pkt[Ether].src and pkt[ARP].hwdst == pkt[Ether].dst:
+    def packet_is_in_acl(self, pkt):
+        return True
+
+    def response_has_ip_bind_to_mac_broadcast(self, pkt):
+        return pkt[ARP].hwsrc == 'ff:ff:ff:ff:ff:ff'
+
+    def response_linklayer_address_matches_arp(self, pkt):
+        if pkt[ARP].hwsrc == pkt[Ether].src:
             return True
-        elif pkt[ARP].op == ARP.who_has and pkt[ARP].hwsrc == pkt[Ether].src:
+
+    def request_linklayer_address_matches_arp(self, pkt):
+        if pkt[ARP].hwsrc == pkt[Ether].src and pkt[ARP].hwdst == pkt[Ether].dst:
             return True
-        else:
-            return False
 
     def response_is_send_to_unicast(self, pkt) -> bool:
-        if pkt[ARP].op == ARP.is_at:
-            return pkt[ARP].hwdst != '00:00:00:00:00:00'
-        else:
-            return True
+        return pkt[Ether].hwdst != 'ff:ff:ff:ff:ff:ff'
 
     def request_is_send_to_broadcast(self, pkt) -> bool:
-        if pkt[ARP].op == ARP.who_has:
-            return pkt[ARP].hwdst == '00:00:00:00:00:00'
-        else:
-            return True
+        return pkt[Ether].hwdst == 'ff:ff:ff:ff:ff:ff'
 
     def has_valid_arp_src_mac_address(self, pkt) -> bool:
         return is_valid_mac_address(pkt[ARP].hwsrc)
--- Project 2/modules/arp/test_arp_module.py
+++ Project 2/modules/arp/test_arp_module.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from scapy.layers.l2 import Ether, ARP
 import pytest
 
@@ -23,7 +25,13 @@ pdst= 172.16.20.255
 @pytest.fixture
 def arp():
     from modules.arp.arp_module import ARPModule
-    return ARPModule()
+    return ARPModule(acl_conf=None)
+
+
+@pytest.fixture
+def arp_acl():
+    from modules.arp.arp_module import ARPModule
+    return ARPModule(acl_conf=Path('example_acl_config.txt'))
 
 
 def test_error_on_improperly_formatted_packet(arp):
@@ -31,7 +39,7 @@ def test_error_on_improperly_formatted_packet(arp):
 
 
 def test_error_on_binding_to_broadcast(arp):
-    a = ARP(hwsrc='00:11:22:aa:bb:cc', hwdst='ff:ff:ff:ff:ff:ff', op='is-at')
+    a = ARP(hwsrc='ff:ff:ff:ff:ff:ff', hwdst='00:11:22:aa:bb:cc', op='is-at')
     response = arp.receive_packet(a)
     assert type(response) is ErrorResponse
 
@@ -49,14 +57,16 @@ def test_error_on_invalid_arp_mac_addresses(arp):
 
 
 def test_notice_on_request_not_sent_to_broadcast(arp):
+    e = Ether(src='00:11:22:aa:bb:cd', dst='00:11:22:aa:bb:cc')
     a = ARP(hwsrc='00:11:22:aa:bb:cc', hwdst='00:11:22:aa:bb:ca', op='who-has')
-    response = arp.receive_packet(a)
+    response = arp.receive_packet(e / a)
     assert type(response) is NoticeRespone
 
 
 def test_notice_on_response_not_sent_to_unicast(arp):
+    e = Ether(src='00:11:22:aa:bb:cd', dst='ff:ff:ff:ff:ff:ff')
     a = ARP(hwsrc='00:11:22:aa:bb:cc', hwdst='00:00:00:00:00:00', op='is-at')
-    response = arp.receive_packet(a)
+    response = arp.receive_packet(e / a)
     assert type(response) is NoticeRespone
 
 
