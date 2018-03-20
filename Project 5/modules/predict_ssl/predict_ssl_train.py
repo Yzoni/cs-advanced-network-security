dfd343c76dd4ec078c66328117fc8202c6ff2348
Yorick de Boer <mail@yorickdeboer.nl>
2018-03-20 14:41:04 +0100

    [code5] auto browse

--- Project 5/modules/predict_ssl/predict_ssl_train.py
+++ Project 5/modules/predict_ssl/predict_ssl_train.py
@@ -1,18 +1,20 @@
 import subprocess
-from multiprocessing import Process
 import argparse
 from pathlib2 import Path
 import os
-
+from selenium import webdriver
+from random import choice
+import time
 
 import pexpect
+from selenium.common.exceptions import WebDriverException
 
 dir_path = Path(os.path.dirname(os.path.realpath(__file__)))
 
 apps = [
-    'www.nrc.nl',
+    'about.gitlab.com',
     'docs.python.org',
-    'stackoverflow.com',
+    'edition.cnn.com',
     'news.ycombinator.com',
     'www.tudelft.nl'
 ]
@@ -22,17 +24,65 @@ def start_surfing(app):
     subprocess.call(str(dir_path / 'randomSurfer.sh ') + app, shell=True)
 
 
+def click_link(driver, links, visited):
+    l = choice(links)
+    link_url = l.get_attribute('href')
+
+    try:
+        print('Visiting: {}'.format(link_url))
+        l.click()
+        visited.append(link_url)
+    except WebDriverException:
+        print('Could not click link: {}'.format(link_url))
+        visited.append(link_url)
+        click_link(driver, links, visited)
+
+    return visited
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='SSL fingerprint trainer')
     parser.add_argument('interface', type=str,
                         help='Interface to capture on')
+    parser.add_argument('--traces-per-app', dest='ntraces', type=int, default=1,
+                        help='Traces per app')
+    parser.add_argument('--time-per-trace', dest='time', type=int, default=15,
+                        help='Time per trace')
     args = parser.parse_args()
 
+    driver = webdriver.Firefox()
     for app in apps:
+        for trace in range(args.ntraces):
 
-        p_capture = pexpect.spawn('tcpdump', ['tcp', 'port', str(443), '-i', str(args.interface), '-w', str(app + '.pcap')])
+            p_capture = pexpect.spawn('tcpdump',
+                                      ['tcp', 'port', str(443), '-i', str(args.interface), '-w',
+                                       str('{}-{}.pcap').format(app, trace)])
 
-        subprocess.call(str(dir_path / 'randomSurfer.sh ') + app, shell=True)
+            visited = list()
 
-        p_capture.sendcontrol('c')
-        p_capture.close()
+            app_url = 'https://' + app
+            print('Visiting: {}'.format(app_url))
+            driver.get(app_url)
+            visited.append(app_url)
+
+            start_time = time.time()
+            while True:
+                links = driver.find_elements_by_tag_name('a')
+                links = list(filter(
+                    lambda x: str(x.get_attribute('href')).startswith(app_url) and str(
+                        x.get_attribute('href')) not in visited, links))
+
+                if len(links) < 1:
+                    print('Zero links, going back')
+                    driver.back()
+                    continue
+
+                visited += click_link(driver, links, visited)
+
+                if time.time() - start_time > args.time:
+                    break
+
+            p_capture.sendcontrol('c')
+            p_capture.close()
+
+    driver.close()
--- Project 5/run.sh
+++ Project 5/run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-sudo apt -y install libpcap-dev python-pip git graphviz
+sudo apt -y install libpcap-dev python-pip git graphviz firefox
 
 sudo pip3 install -r requirements.txt
+
+wget https://github.com/mozilla/geckodriver/releases/download/v0.20.0/geckodriver-v0.20.0-linux64.tar.gz
+tar -xvzf geckodriver-v0.20.0-linux64.tar.gz
+chmod +x geckodriver
+sudo mv geckodriver /usr/local/bin/
 
 python3 ips.py $1 $2
